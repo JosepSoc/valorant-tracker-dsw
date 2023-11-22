e851cbb9b5d59bb1f94ff08fa673071c74c87a84
JosepSoc <jose.socolsky@gmail.com>
2023-11-22 18:31:24 -0300

    Abm-list mobile terminado, todo: abm-form

---
+++ front-vtracker/src/app/abm-form/abm-form.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-abm-form',
+  templateUrl: './abm-form.component.html',
+  styleUrls: ['./abm-form.component.scss']
+})
+export class AbmFormComponent {
+
+}
--- front-vtracker/src/app/app.module.ts
+++ front-vtracker/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AbmListComponent } from './abm-list/abm-list.component';
+import { AbmFormComponent } from './abm-form/abm-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AbmListComponent
+    AbmListComponent,
+    AbmFormComponent
   ],
   imports: [
     BrowserModule,
