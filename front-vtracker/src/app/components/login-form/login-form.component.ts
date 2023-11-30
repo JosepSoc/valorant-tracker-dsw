fbbd6e31c6f6450c316b90f4cbbcab12b084a6fd
JosephSoc <jose.socolsky@gmail.com>
2023-11-30 20:16:02 -0300

    Added some functinality, unfinished

--- front-vtracker/src/app/components/login-form/login-form.component.ts
+++ front-vtracker/src/app/components/login-form/login-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
   selector: 'app-login-form',
@@ -7,7 +7,30 @@ import { Component } from '@angular/core';
 })
 export class LoginFormComponent {
 
-  turnLogIn(){
+  @ViewChild('logHeader') logHeader!: ElementRef;
+  @ViewChild('usernameBox') usernameBox!: ElementRef;
+  @ViewChild('passwordBox') passwordBox!: ElementRef;
+  @ViewChild('confirmpassBox') confirmpassBox!: ElementRef;
+  @ViewChild('emailBox') emailBox!: ElementRef;
+  @ViewChild('termsCheckBox') termsCheckBox!: ElementRef;
+  @ViewChild('registerBtn') registerBtn!: ElementRef;
+  @ViewChild('gotoLoginBtn') gotoLoginBtn!: ElementRef;
+  
+  isRegister: boolean = false;
+
+  ngAfterViewInit(){
+    if(this.isRegister === true){
+      this.logHeader.nativeElement.innerHTML = "Sign up";
+    }
+    else{
+      this.logHeader.nativeElement.innerHTML = "Log in";
+    }
+  }
+  turnLogin(){
+
+  }
+
+  turnRegister(){
     
   }
 }
