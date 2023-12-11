43d2a80c7f5f75d82bbb6525de1bac2f9dbc7548
JosephSoc <jose.socolsky@gmail.com>
2023-12-11 05:00:18 -0300

    added cors for local api request

--- back-vtracker/src/app.ts
+++ back-vtracker/src/app.ts
@@ -4,10 +4,15 @@ import { weaponRouter } from './weapons/weapon.routes.js';
 import { agentRouter } from './agents/agent.routes.js';
 import { abilityRouter } from './abilities/abilities.routes.js';
 import { rolRouter } from './rols/rols.routes.js';
+import cors from 'cors';
 
 import { weaponTypeRouter } from './weaponType/weaponType.routes.js';
 const app = express();
 app.use(express.json());
+
+app.use(cors({
+  origin: 'http://localhost:4200'
+}));
 
 app.use('/api/rols', rolRouter);
 app.use('/api/abilities', abilityRouter);
@@ -15,6 +20,7 @@ app.use('/api/users', userRouter);
 app.use('/api/agents', agentRouter)
 app.use('/api/weapons', weaponRouter);
 app.use('/api/weaponTypes', weaponTypeRouter);
+
 
 app.use((_, res) => {
   res.status(404).send({ message: 'Resource not found' });
